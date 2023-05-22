c8f717fccbf499bb8daf59af5da388d45b6490f3
TimofeyKornilin <you@example.com>
2023-05-22 16:16:50 +0700

    add Random

--- Examp003_SUM/Program.cs
+++ Examp003_SUM/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 23;
-int numberB = 34;
+﻿int numberA = new Random().Next(1,10); // all number from 1 to 9 includ
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine (numberB);
 Console.WriteLine(numberA+numberB);
