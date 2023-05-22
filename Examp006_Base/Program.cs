a5a83a650c1c1f669b7bd9712bbd17768fe8c53f
TimofeyKornilin <you@example.com>
2023-05-22 17:18:07 +0700

    add program for finding the largest number from a set

---
+++ Examp006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 3;
+int c = 8;
+int e = 26;
+int d = 1;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (e > max) max = e;
+if (d > max) max = d;
+
+Console.Write("max = ");
+Console.WriteLine(max);
