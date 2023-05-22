46923a67d04a62201f2ca736ba7d074932ced116
TimofeyKornilin <you@example.com>
2023-05-22 17:00:39 +0700

    add if else program

---
+++ Examp005_if_else/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("*** Start Program ***");
+Console.WriteLine(" ");
+
+Console.Write("Enter your name: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "masha")
+{
+    Console.WriteLine("Yoo! This is Masha!");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.Write(userName);
+    Console.WriteLine("!");
+}
+
+Console.WriteLine(" ");
+Console.WriteLine("*** END Program ***");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
