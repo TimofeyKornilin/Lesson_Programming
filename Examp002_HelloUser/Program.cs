b5dd24a92612de9810e264585303e18d237a917d
TimofeyKornilin <you@example.com>
2023-05-22 14:01:19 +0700

    add Examp002 Hello User

---
+++ Examp002_HelloUser/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("*** Start of program ***");
+Console.WriteLine(" ");
+
+Console.Write("Enter your name: ");
+string userName = Console.ReadLine();
+
+Console.Write("Hello, ");
+Console.Write(userName);
+Console.WriteLine("!");
+
+Console.WriteLine(" ");
+Console.WriteLine("*** END of program ***");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
